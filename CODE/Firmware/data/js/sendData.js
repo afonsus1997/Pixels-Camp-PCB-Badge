479fa88f216c1499eafd428d20dbd46bb3eb69a0
Afonso Muralha <afonsomuralha@gmail.com>
2021-12-20 22:14:14 +0000

    web improvements

--- CODE/Firmware/data/js/sendData.js
+++ CODE/Firmware/data/js/sendData.js
@@ -54,6 +54,12 @@ function brightness_button_handler(){
     xhttp.send();
 }
 
+function set_test(){
+  var formString  = document.getElementById('displaytext');
+  formString.value = "We're no strangers to love, You know the rules and so do I, A full commitment's what I'm thinking of, You wouldn't get this from any other guy, I just wanna tell you how I'm feeling, Gotta make you understand. Never gonna give you up, Never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, Never gonna say goodbye, Never gonna tell a lie and hurt you.";
+  formString.focus();
+}
+
 
 
 
---
+++ CODE/Firmware/include/wifi_credentials.h
@@ -0,0 +1,3 @@
+
+const char* SSID = "YOUR_SSID";
+const char* PASSWORD = "YOUR_PASSWORD";
